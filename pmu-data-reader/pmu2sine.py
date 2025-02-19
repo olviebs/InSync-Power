7c1f26502ed431f38e530884423efedae6410b41
GustavoMoranDiaz <gmorandi@stevens.edu>
2025-02-19 14:01:39 -0500

    added a show graph option for less clutter

--- pmu-data-reader/pmu2sine.py
+++ pmu-data-reader/pmu2sine.py
@@ -4,13 +4,16 @@ import matplotlib.animation as animation
 import pandas as pd
 import time
 
+showGraph = False
+
 df = pd.read_csv('Bus2_Competition_Data.csv', usecols=['BUS2_VA_ANG', 'BUS2_VA_MAG', 'BUS2_Freq']) # df.iat[0,0] = ANG, df.iat[0,1] = MAG, df.iat[0,2] = FREQ
 
 T0 = 0 # start time
+TE = 0.033 # time after 1 30hz cycle
 
 PI = np.pi
 
-TIMERES = 30300
+TIMERES = 1000 # number of values per 2 cycles of 60hz output wave
 
 ROW = 0
 
@@ -18,13 +21,12 @@ FREQ = df.iat[ROW,2] # initial frequency
 ANG = df.iat[ROW,0] # initial angle
 MAG = df.iat[ROW,1] # initial magnitute
 
-TE = 0.033 # corresponds to PMU sampling rate
-
 fig = plt.figure()
 
 ax1 = fig.add_subplot(1,1,1)
 
-ts = np.arange(T0, TE, (1/TIMERES))
+ts = np.linspace(T0, TE, 1000)
+print(len(ts))
 
 def waveConditioning (mag, freq, ang):
     global PI
@@ -34,19 +36,32 @@ def waveConditioning (mag, freq, ang):
     ys = ys.astype(int)
     return ys
 
-def animate(i):
-    global T0, TE, FREQ, MAG, ROW, ANG
-    ys = waveConditioning(MAG, FREQ, ANG)
-    ax1.clear()
-    ax1.plot(ts,ys)
-    for i in range(0,int(TE*TIMERES)+1):
-        print(ys[i])
-        time.sleep(1/TIMERES)
-    ROW += 1
-    MAG = df.iat[ROW,1]
-    FREQ = df.iat[ROW,2]
-    ANG = df.iat[ROW,0]
-    
-# code to initialize visualizer. Not required for actual use, comment out when testing with device
-ani = animation.FuncAnimation(fig,animate, interval=33) # set to 33 interval for accurate readings
-plt.show()
+if showGraph == True:
+    def animate(i):
+        global T0, TE, FREQ, MAG, ROW, ANG, TIMERES
+        ys = waveConditioning(MAG, FREQ, ANG)
+        ax1.clear()
+        ax1.plot(ts,ys)
+        for i in range(0,TIMERES):
+            print(ys[i])
+            time.sleep(TE/TIMERES)# when using raspberry pi, switch this to nanosleep functions
+        ROW += 1
+        MAG = df.iat[ROW,1]
+        FREQ = df.iat[ROW,2]
+        ANG = df.iat[ROW,0]
+    # code to initialize visualizer. Not required for actual use, comment out when testing with device
+    ani = animation.FuncAnimation(fig,animate, interval=33) # interval dictates how much time between graph updates in ms
+    plt.show()
+elif showGraph == False:
+    while True:
+        ys = waveConditioning(MAG, FREQ, ANG)
+        timestart = time.time_ns()
+        for i in range(0,TIMERES):
+            print(f"{ys[i]}:{time.time_ns()}")
+            time.sleep(TE/TIMERES) # when using raspberry pi, switch this to nanosleep functions
+        ROW += 1
+        MAG = df.iat[ROW,1]
+        FREQ = df.iat[ROW,2]
+        ANG = df.iat[ROW,0]
+else:
+    print("Please select True or False for showGraph in line 7")
